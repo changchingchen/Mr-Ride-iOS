d719b661cbf1459564dd8f31be928686db3e4af9
admin <admin@AppWorks-gongyongdiannao.local>
2016-05-30 20:29:31 +0800

    Add Core data files (CRUD not implemented yet)

---
+++ Mr-Ride-iOS/Record.swift
@@ -0,0 +1,17 @@
+//
+//  Record.swift
+//  Mr-Ride-iOS
+//
+//  Created by admin on 5/30/16.
+//  Copyright © 2016 AppWorks School Snakeking. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Record: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
